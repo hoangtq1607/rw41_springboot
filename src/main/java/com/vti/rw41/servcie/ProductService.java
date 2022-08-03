59387851ddb60083738323d966a16051aa89a771
hoang tran <hoangtq1607@gmail.com>
2022-08-03 22:11:14 +0700

    udpate coe

--- src/main/java/com/vti/rw41/servcie/ProductService.java
+++ src/main/java/com/vti/rw41/servcie/ProductService.java
@@ -1,14 +1,11 @@
 package com.vti.rw41.servcie;
 
 import com.vti.rw41.dto.response.ProductDTO;
-import com.vti.rw41.entity.ProductEntity;
 import com.vti.rw41.reposioty.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.function.Function;
 
 @Service
 public class ProductService {
